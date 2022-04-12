d533db0ac52f9eb008fbbde4117ac7acb3d31659
Kanishka Shah <kanishkashah25@gmail.com>
2022-04-12 16:02:16 +0530

    strings and datatypes

---
+++ c++/01_data_types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    char grade ='A';
+    string phase =" awesome";
+    int num= 789;
+    double gpa = 3.456;
+    bool ismale = "false";
+
+    cout<<grade<<endl;
+    cout<<phase<<endl;
+    return 0;
+}
---
+++ c++/01_story.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    string characterName = "john";
+    int characterAge;
+    characterAge = 35; 
+    cout<<"their was once a man named "  <<  characterName  <<endl;
+    cout<<"he was " << characterAge <<  "years old"<<endl;
+    characterName = "mike";
+    cout<<"he liked his name " << characterName <<endl;
+    cout<<"but he did not like being 70 years old"<<endl;
+    return 0;
+
+}
