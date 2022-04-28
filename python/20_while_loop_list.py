b52f008fd9beaa41b407493ad28ed0f2e117c82b
Kanishka Shah <kanishkashah25@gmail.com>
2022-04-28 18:07:59 +0530

    files added

---
+++ python/20_while_loop_list.py
@@ -0,0 +1,8 @@
+fruits=['banana','mangoes','watermelon','muskmelon']
+i=0
+
+while i<len(fruits) :
+    print (fruits[i])
+    i=i+1
+    
+     
---
+++ python/21_for_loop.py
@@ -0,0 +1,12 @@
+fruits=['banana','mango','watermelon','muskmelon','orange']
+
+
+for item in fruits :
+    print(item)
+
+
+
+
+
+
+
