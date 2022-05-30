c964b8747d8e0d26041cf5e2bffeebfb5f24a3a9
Kanishka Shah <kanishkashah25@gmail.com>
2022-05-30 18:52:59 +0530

    files added

---
+++ Web Developement/javascript/index.js
@@ -0,0 +1,7 @@
+var myname ="kanishka shah";
+var yourname ="john";
+var myvar ="abc";
+var my123 =123;
+var Myname ="Kanishka Shah";
+var my_name ="Kanishka";
+var UserScoreFinal =123;
---
+++ Web Developement/javascript/string_concatenation.js
@@ -0,0 +1,3 @@
+var message =" Hello";
+var Myname = "John Bauer";
+alert(message + " " + "there ," + " " + Myname);
