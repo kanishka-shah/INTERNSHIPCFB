432a79360c159aa3b69ffe3235e81b601b3b15d1
Kanishka Shah <kanishkashah25@gmail.com>
2022-06-07 18:37:30 +0530

    files added

--- Web Developement/dice_challenge/index.js
+++ Web Developement/dice_challenge/index.js
@@ -1,1 +1,29 @@
-alert("working");
+
+var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceimg = "img" + randomnumber1 + ".jpg";
+var randomImageSource = "images/" + randomDiceimg;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/img" + randomnumber2 + ".jpg";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomnumber1>randomnumber2)
+{
+    document.querySelectorAll("h1").innerHTML = "player1 wins!";
+
+}
+
+else if(randomnumber2>randomnumber1)
+{
+    document.querySelectorAll("h1").innerHTML = "player2 wins!";
+}
+
+else
+{
+    document.querySelectorAll("h1").innerHTML = "draw!";
+}
+
+
+
