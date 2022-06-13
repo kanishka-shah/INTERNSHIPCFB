b9e3b2822e5d7f1e85226f89e7a068feec02663c
Kanishka Shah <kanishkashah25@gmail.com>
2022-06-13 17:40:13 +0530

    files added

---
+++ intro-to-node/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+fs.copyFileSync("file1.txt" , "file2.txt");
+
+const { characters } = require("marvel-characters");
+
+var superheroes = require("superheroes");
+var mySuperheroname = superheroes.random();
+console.log(mySuperheroname);
+
+
+
+
+
+
+
+
+
